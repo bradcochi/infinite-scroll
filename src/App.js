5fe753b1e81637610869b6be42ef581f0bdf2b6a
Brad <bradcochi@gmail.com>
2024-01-31 15:53:00 -0700

    fixed errors in return statement

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { useState, useEfect } from "react";
 import "./index.css";
 
-export default function App() {
+function App() {
+  // set state variables
   const [photoArray, setPhotoArray] = useState([]);
-  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState([]);
   const [totalImages, setTotalImages] = useState(0);
   const [ready, setReady] = useState(false);
   const [count, setCount] = useState(5);
 
-  const getPhotos = async () => {
-    const apiKey = "0NO6cOHvVei9ZEnf46-e_-_XoNoamNU_UITgcOFqSPg";
-    const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+  // const getPhotos = async () => {
+  //   const apiKey = "0NO6cOHvVei9ZEnf46-e_-_XoNoamNU_UITgcOFqSPg";
+  //   const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-    return (
-      <div>
-        <p>Hello World</p>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  );
 }
+
+export default App;
