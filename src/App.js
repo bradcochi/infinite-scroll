59a293a704e4ab32576089e1d260ec4b20b77c05
Brad <bradcochi@gmail.com>
2024-02-03 12:43:10 -0700

    infinite scroll mostly working but not accounting for loading and running too many times

--- src/App.js
+++ src/App.js
@@ -1,75 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./index.css";
 
 function App() {
   const [photoArray, setPhotoArray] = useState([]);
-  const [imagesLoaded, setImagesLoaded] = useState(0);
-  const [totalImages, setTotalImages] = useState(0);
-  const [ready, setReady] = useState(false);
-  const [count, setCount] = useState(5);
-
-  const getPhotos = async () => {
-    const apiKey = "0NO6cOHvVei9ZEnf46-e_-_XoNoamNU_UITgcOFqSPg";
-    const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setPhotoArray((prevPhotoArray) => [...prevPhotoArray, ...data]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const [loading, setLoading] = useState(false);
 
   console.log({ photoArray });
 
-  const imageLoaded = () => {
-    setImagesLoaded((prevImagesLoaded) => prevImagesLoaded + 1);
-    if (imagesLoaded === totalImages) {
-      setReady(true);
-      setCount(5);
-    }
-  };
+  const getPhotos = useCallback(
+    async (abortController) => {
+      if (loading === false) {
+        setLoading(true);
+        const apiKey = "0NO6cOHvVei9ZEnf46-e_-_XoNoamNU_UITgcOFqSPg";
+        const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=5`;
+
+        try {
+          const response = await fetch(url, {
+            signal: abortController.signal,
+          });
+          const data = await response.json();
+          setPhotoArray((prevPhotoArray) => [...prevPhotoArray, ...data]);
+        } catch (error) {
+          console.error(error);
+          if (error.name !== "AbortError") {
+            console.error("AbortError");
+          }
+        }
+        setLoading(false);
+      }
+    },
+    [loading]
+  );
 
   useEffect(() => {
-    getPhotos();
-  }, []);
+    const abortController = new AbortController();
 
-  useEffect(() => {
     const handleScroll = () => {
-      console.log("Hit");
-      console.log("window.scrollY", window.scrollY);
-      console.log("window.innerHeight", window.innerHeight);
-      console.log("document.body.offsetHeight", document.body.offsetHeight);
-      console.log("ready", ready);
+      // console.log("Hit");
+      // console.log("window.scrollY", window.scrollY);
+      // console.log("window.innerHeight", window.innerHeight);
+      // console.log("document.body.offsetHeight", document.body.offsetHeight);
+
       if (
         window.scrollY + window.innerHeight >=
-          document.body.offsetHeight - 1000 &&
-        ready
+        document.body.offsetHeight - 1000
       ) {
-        getPhotos();
-        setReady(false);
+        getPhotos(abortController);
       }
     };
 
+    getPhotos(abortController);
+
     window.addEventListener("scroll", handleScroll);
 
-    // clean up by removing the event listener when the component unmounts
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      abortController.abort();
     };
-  }, [ready]);
-
-  useEffect(() => {
-    setTotalImages(photoArray.length);
-  }, [photoArray]);
+  }, []);
 
   return (
     <div>
       <h1 className="heading">Infinity Scroll - Unsplash API</h1>
-      <div className="loader" style={{ display: ready ? "none" : "block" }}>
+      {/* {<div className="loader" style={{ display: ready ? "none" : "block" }}>
         Loading...
-      </div>
+      </div>} */}
       <div className="image-container">
         {photoArray.map((data, index) => (
           <a
@@ -82,7 +77,6 @@ function App() {
               src={data.urls.regular}
               alt={data.alt_description || "Unknown"}
               title={data.alt_description || "Unknown"}
-              onLoad={imageLoaded}
             />
           </a>
         ))}
